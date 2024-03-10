269ef31569710a3da412d6834334ab434192116c
akhil <akhilboyanapalli657@gmail.com>
2024-03-09 20:07:04 -0600

    working on numbers

---
+++ Basics/numericTypes.py
@@ -0,0 +1,44 @@
+a = 10;
+b = 999;
+c = -2;
+print(a,b,c)
+print(type(a))
+
+x=33.5;
+y=99.8;
+z = 333.0;
+print(x,y,z)
+print(type(z))
+
+c1=3+2j;
+print(type(c1))
+
+b1 = 0b1101;
+print(b1)
+print(type(b1))
+
+h = 0xffa;
+print(h)
+print(type(h))
+
+o = 0o1234;
+print(o);
+print(type(o))
+
+bl= True;
+print(bl)
+print(type(bl))
+
+print(9<8)
+
+t1 = int(x)
+print(t1)
+print(type(h))
+
+t2 = float('22.5')
+print(t2)
+print(type(t2))
+
+print(bin(13))
+
+print(oct(13))
