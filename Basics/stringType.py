9b724d73fe735c6fa7c287cf5a2468020a08e60b
akhil <akhilboyanapalli657@gmail.com>
2024-03-10 09:55:20 -0500

    String data type

---
+++ Basics/stringType.py
@@ -0,0 +1,40 @@
+s="   You are awesome    "
+print(s)
+
+s1 = """You are
+the
+creator of your Destiny
+"""
+print(s1)
+
+print(s[2])
+
+print(s * 3)
+
+print(len(s1))
+
+print(s[0:5])
+
+print(s[0:])
+
+print(s[:8])
+
+print(s[-3:-1])
+
+print(s[0:9:3])
+print(s[15::-1])
+print(s[::-1])
+
+print(s.strip())
+print(s.lstrip())
+print(s.rstrip())
+
+print(s.find('awe',0,8))
+
+print(s.count('a'))
+
+print(s.replace("awesome","super"))
+
+print(s.upper())
+print(s.lower())
+print(s.title())
