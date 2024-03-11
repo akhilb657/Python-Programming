20a1eaf7fcd9b24667204acb61741efa9b321c98
akhil <akhilboyanapalli657@gmail.com>
2024-03-11 18:36:30 -0500

    working with tuple

---
+++ Basics/tupleType.py
@@ -0,0 +1,16 @@
+tpl = (20,30,22,20,34,'abc')
+#tpl[0] = 123;
+print(type(tpl))
+print(tpl*3)
+
+print(tpl.count(20))
+
+print(tpl.index('abc'))
+
+print(tpl[1:])
+
+lst = [67,33,44,77,'xyz']
+print(type(lst))
+tpl1 = tuple(lst)
+print(type(tpl1))
+print(tpl1)
