35cfd44426cc4f0827b36ae505ef6a7b211ab95d
akhil <akhilboyanapalli657@gmail.com>
2024-03-13 19:09:15 -0500

    summary of all DataTypes

---
+++ Basics/summary.py
@@ -0,0 +1,40 @@
+a = 20
+b = 20
+b = 30
+print(id(a),id(b))
+
+f = 3.14
+c = 5 + 3j
+print(c.real, c.imag)
+
+bool = True
+
+s = "Akhil"
+print(type(s))
+
+lst = [1,2,3,4]
+print(type(lst))
+b = bytes(lst)
+print(type(b))
+
+ba = bytearray(lst)
+print(type(ba))
+
+r1 = range(20)
+r2 = range(20)
+range(1,20)
+range(1,20,3)
+
+t = (1,2,3,4,5)
+print(type(t))
+
+s = {1,2,3,5,8,9,3,2}
+print(type(s))
+print(s)
+
+fs = frozenset(s)
+print(type(fs))
+
+d = {1:100,2:99,3:99}
+print(type(d))
+
