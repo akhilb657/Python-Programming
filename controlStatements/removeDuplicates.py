acda7e794f00a78cbb9b2566839c4fff0b5dc7ba
akhil <akhilboyanapalli657@gmail.com>
2024-03-17 16:19:30 -0500

    Remove duplicates from list of elements

---
+++ controlStatements/removeDuplicates.py
@@ -0,0 +1,15 @@
+lst = eval(input("Enter list of elements"))
+
+print(lst)
+'''
+l2 = []
+for each_value in lst:
+    if each_value not in l2:
+        l2.append(each_value)
+
+print(l2)
+'''
+
+s = set(lst)
+
+print(s)
