b3435b8ec770b22088d35b778ae51dee1cc15f50
akhil <akhilboyanapalli657@gmail.com>
2024-03-18 18:30:39 -0500

    Reverse a string

---
+++ controlStatements/reverseString.py
@@ -0,0 +1,17 @@
+#s = input("Enter a string: ")
+#print(s[::-1])
+
+'''
+s = input("Enter a string: ")
+s1 = ''
+i=len(s)-1
+while i>=0:
+    s1 = s1 + s[i]
+    i -= 1
+print(s1)
+'''
+
+#s = ''.join(['a','b','c'])
+#print(s)
+s = input("Enter a string: ")
+print(''.join(reversed(s)))
